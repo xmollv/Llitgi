45569d85fa9f35beeb897de882ac8184f08ec2d7
Xavi Moll <xmollv@gmail.com>
2017-12-24 19:54:06 +0100

    Added the base project

---
+++ ReadLater/ReadLater/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ReadLater
+//
+//  Created by Xavi Moll on 24/12/2017.
+//  Copyright © 2017 xmollv. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.tintColor = .black
+        let rootViewController = UITabBarController()
+        window?.rootViewController = rootViewController
+        
+        return true
+    }
+
+}
+
