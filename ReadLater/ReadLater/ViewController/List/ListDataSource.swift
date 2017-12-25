9783d8a2cbf4aab24f8c3e220a2e395773c102c0
Xavi Moll <xmollv@gmail.com>
2017-12-25 23:23:14 +0100

    Basic implementation of the UITableViewDataSource

---
+++ ReadLater/ReadLater/ViewController/List/ListDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  ListDataSource.swift
+//  ReadLater
+//
+//  Created by Xavi Moll on 25/12/2017.
+//  Copyright © 2017 xmollv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListDataSource: NSObject, UITableViewDataSource {
+    
+    private var articles: [Article] = []
+    
+    //MARK:- UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
