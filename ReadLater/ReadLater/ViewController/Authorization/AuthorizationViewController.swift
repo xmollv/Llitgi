da065605663a1afa8e892ac4891885ab90a6c87d
Xavi Moll <xmollv@gmail.com>
2017-12-25 12:10:06 +0100

    Added some extensions on UIView and created the basic UI for the Auth process

--- ReadLater/ReadLater/ViewController/Authorization/AuthorizationViewController.swift
+++ ReadLater/ReadLater/ViewController/Authorization/AuthorizationViewController.swift
@@ -8,10 +8,38 @@
 
 import UIKit
 
-class AuthorizationViewController: ViewController {
+enum OAuthStep {
+    case notStarted
+    case requestToken
+    case webOAuth
+    case authorizeToken
+    case finished
+}
 
+class AuthorizationViewController: ViewController {
+    
+    //MARK:- IBOutlets
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var actionButton: UIButton!
+    
+    //MARK:- Private properties
+    private var state: OAuthStep = .notStarted
+
+    //MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupLocalizedStrings()
+    }
+    
+    //MARK: IBActions
+    @IBAction private func actionButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: Private methods
+    private func setupLocalizedStrings() {
+        self.descriptionLabel.text = NSLocalizedString("Hey there! We need your permission to access your Pocket list. To do so, simply tap the button below.", comment: "")
+        self.actionButton.setTitle(NSLocalizedString("Let's do it!", comment: ""), for: .normal)
     }
 
 }
