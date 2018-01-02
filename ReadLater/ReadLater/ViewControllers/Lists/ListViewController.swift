b93df58d8ba52ebf41776e94239eb6f0a6734657
Xavi Moll <xmollv@gmail.com>
2018-01-02 19:20:57 +0100

    Added the basic implementation for pagination and saved the heights of the cells to avoid scrolling jumps

--- ReadLater/ReadLater/ViewControllers/Lists/ListViewController.swift
+++ ReadLater/ReadLater/ViewControllers/Lists/ListViewController.swift
@@ -26,10 +26,16 @@ class ListViewController: ViewController {
     private let typeOfList: TypeOfList
     private var dataSource: ListDataSource?
     private var swipeActionManager: ListSwipeActionManager?
+    private var cellHeights: [IndexPath : CGFloat] = [:]
+    
+    // Pagination
+    private var pageToRequest = 0
+    private var isLastPage = false
+    private var isCurrentlyFetching = false
     
     private lazy var refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(self.fetchList), for: .valueChanged)
+        refreshControl.addTarget(self, action: #selector(self.pullToRefresh), for: .valueChanged)
         return refreshControl
     }()
     
@@ -88,25 +94,38 @@ class ListViewController: ViewController {
         self.tableView.refreshControl = self.refreshControl
     }
     
-    @objc private func fetchList() {
+    @objc private func pullToRefresh() {
+        self.pageToRequest = 0
+        self.isLastPage = false
+        self.fetchList()
+    }
+    
+    private func fetchList() {
+        self.isCurrentlyFetching = true
         let endpoint: PocketAPIEndpoint
         switch self.typeOfList {
         case .myList:
-            endpoint = .getList
+            endpoint = .getList(page: self.pageToRequest)
         case .favorites:
-            endpoint = .getFavorites
+            endpoint = .getFavorites(page: self.pageToRequest)
         case .archive:
-            endpoint = .getArchive
+            endpoint = .getArchive(page: self.pageToRequest)
         }
+        
         self.dataProvider.perform(endpoint: endpoint) { [weak self] (result: Result<[CoreDataItem]>) in
             guard let strongSelf = self else { return }
             switch result {
-            case .isSuccess:
+            case .isSuccess(let items):
+                strongSelf.pageToRequest += 1
+                if items.count == 0 {
+                    strongSelf.isLastPage = true
+                }
                 Logger.log("Succes on: \(strongSelf.typeOfList)")
             case .isFailure(let error):
                 Logger.log("Error on: \(strongSelf.typeOfList).\n\n Error: \(error)", event: .error)
             }
             strongSelf.refreshControl.endRefreshing()
+            strongSelf.isCurrentlyFetching = false
         }
     }
     
@@ -151,6 +170,24 @@ class ListViewController: ViewController {
 
 //MARK:- UITableViewDelegate
 extension ListViewController: UITableViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if self.tableView.contentOffset.y > (self.tableView.contentSize.height - self.tableView.bounds.size.height) {
+            guard self.isLastPage == false else { return }
+            guard self.isCurrentlyFetching == false else { return }
+            self.fetchList()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        self.cellHeights[indexPath] = cell.frame.size.height
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let height = self.cellHeights[indexPath] else { return UITableViewAutomaticDimension }
+        return height
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let sfs = self.safariViewController(at: indexPath) else { return }
         self.present(sfs, animated: true, completion: nil)
