8e0db1cb38dc220db5f502e5a93e45e32f78259d
Xavi Moll <xmollv@gmail.com>
2018-01-07 23:51:37 +0100

    Added more UI for the settings

--- ReadLater/ReadLater/ViewControllers/Settings/SettingsViewController.swift
+++ ReadLater/ReadLater/ViewControllers/Settings/SettingsViewController.swift
@@ -13,11 +13,28 @@ class SettingsViewController: ViewController {
     //MARK:- IBOutlets
     @IBOutlet private var titleLabel: UILabel!
 
+    @IBOutlet private var badgeCountLabel: UILabel!
+    @IBOutlet private var badgeCountExplanationLabel: UILabel!
+    @IBOutlet private var badgeCountSwitch: UISwitch!
+    
+    
     //MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.setupLocalizedStrings()
+    }
+    
+    //MARK:- IBActions
+    @IBAction func badgeCountValueChanged(_ sender: UISwitch) {
+
+    }
+    
+    //MARK:- Private methods
+    private func setupLocalizedStrings() {
         self.titleLabel.text = NSLocalizedString("Settings", comment: "")
+        self.badgeCountLabel.text = NSLocalizedString("Show app badge count", comment: "")
+        self.badgeCountExplanationLabel.text = NSLocalizedString("This will display a badge count on the app icon on your homescreen with the number of elements on your list.", comment: "")
     }
 
 }
