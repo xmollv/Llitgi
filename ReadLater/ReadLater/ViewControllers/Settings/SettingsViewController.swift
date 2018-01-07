219e6e4b770bc607c0e61fbb4b9a6552685f7fc1
Xavi Moll <xmollv@gmail.com>
2018-01-08 00:48:19 +0100

    Added a logout button (UI)

--- ReadLater/ReadLater/ViewControllers/Settings/SettingsViewController.swift
+++ ReadLater/ReadLater/ViewControllers/Settings/SettingsViewController.swift
@@ -17,6 +17,7 @@ class SettingsViewController: ViewController {
     @IBOutlet private var badgeCountExplanationLabel: UILabel!
     @IBOutlet private var badgeCountSwitch: UISwitch!
     
+    @IBOutlet private var logoutButton: UIButton!
     
     //MARK:- Lifecycle
     override func viewDidLoad() {
@@ -26,8 +27,18 @@ class SettingsViewController: ViewController {
     }
     
     //MARK:- IBActions
-    @IBAction func badgeCountValueChanged(_ sender: UISwitch) {
-
+    @IBAction private func badgeCountValueChanged(_ sender: UISwitch) {
+        //TODO: Missing implementation
+        // This should call a manager to trigger for enabling the notifications
+        // The badge should be updated from the 'My List' view controller
+        // or by creating new fetch request that only returns the count of elements on 'My List'
+    }
+    
+    @IBAction private func logoutButtonTapped(_ sender: UIButton) {
+        //TODO: Missing implementation
+        // Delete contents of core data
+        // Delete tokens saved on user defaults
+        // Delete lasSync value stored on user defaults
     }
     
     //MARK:- Private methods
@@ -35,6 +46,7 @@ class SettingsViewController: ViewController {
         self.titleLabel.text = NSLocalizedString("Settings", comment: "")
         self.badgeCountLabel.text = NSLocalizedString("Show app badge count", comment: "")
         self.badgeCountExplanationLabel.text = NSLocalizedString("This will display a badge count on the app icon on your homescreen with the number of elements on your list.", comment: "")
+        self.logoutButton.setTitle(NSLocalizedString("Logout", comment: ""), for: .normal)
     }
 
 }
