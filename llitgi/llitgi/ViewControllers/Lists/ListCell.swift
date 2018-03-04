b8dbd1f84fcb252e4a563e4c1883f70fd55d763f
Xavi Moll <xmollv@gmail.com>
2018-03-04 01:08:38 +0100

    Used SFSafariViewController for the auth to make Apple happy

--- llitgi/llitgi/ViewControllers/Lists/ListCell.swift
+++ llitgi/llitgi/ViewControllers/Lists/ListCell.swift
@@ -15,9 +15,6 @@ class ListCell: UITableViewCell, NibLoadableView {
     @IBOutlet private var titleLabel: UILabel!
     @IBOutlet private var urlLabel: UILabel!
     
-    @IBOutlet private var titleToFavoriteConstraint: NSLayoutConstraint!
-    @IBOutlet private var titleToTrailingConstraint: NSLayoutConstraint!
-    
     //MARK:- Lifecycle
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -31,8 +28,6 @@ class ListCell: UITableViewCell, NibLoadableView {
     
     private func clearCell() {
         self.favoriteView.isHidden = true
-        self.titleToFavoriteConstraint.isActive = false
-        self.titleToTrailingConstraint.isActive = true
         self.titleLabel.text = nil
         self.urlLabel.text = nil
     }
@@ -41,8 +36,6 @@ class ListCell: UITableViewCell, NibLoadableView {
     func configure(with item: Item) {
         if item.isFavorite {
             self.favoriteView.isHidden = false
-            self.titleToTrailingConstraint.isActive = false
-            self.titleToFavoriteConstraint.isActive = true
         }
         self.titleLabel.text = item.title
         self.urlLabel.text = item.url.host
