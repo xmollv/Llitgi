3caa5269f2ab6159e67c96d6e23e0e843c25a1f6
Xavi Moll <xmollv@gmail.com>
2018-03-27 23:42:47 +0200

    Added the background fetch support

--- llitgi/llitgi/AppDelegate.swift
+++ llitgi/llitgi/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     let modelFactory: CoreDataFactory = CoreDataFactoryImplementation()
+    var dataProvider: DataProvider!
     var viewControllerFactory: ViewControllerFactory!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -22,9 +23,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Initialization of dependencies
         let pocketAPI = PocketAPIManager()
-        let dataProvider = DataProvider(pocketAPI: pocketAPI, modelFactory: modelFactory)
+        self.dataProvider = DataProvider(pocketAPI: pocketAPI, modelFactory: modelFactory)
         let userPreferences = UserPreferencesManager()
-        let dependencies = Dependencies(dataProvider: dataProvider, userPreferences: userPreferences)
+        let dependencies = Dependencies(dataProvider: self.dataProvider, userPreferences: userPreferences)
         self.viewControllerFactory = ViewControllerFactory(dependencies: dependencies)
         
         let rootViewController = TabBarController(factory: self.viewControllerFactory)
@@ -69,6 +70,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // The user is not logged in, there's no point in trying to sync anything
+        guard let _ = LlitgiUserDefaults.shared.string(forKey: kAccesToken) else {
+            completionHandler(.noData)
+            return
+        }
+        
+        self.dataProvider.syncLibrary { (result) in
+            switch result {
+            case .isSuccess(let items):
+                // Because this is a sync operation, we just need to care if we get data or not
+                items.isEmpty ? completionHandler(.noData) : completionHandler(.newData)
+            case .isFailure(let error):
+                Logger.log(error.localizedDescription, event: .error)
+                completionHandler(.failed)
+            }
+        }
+    }
 }
-
