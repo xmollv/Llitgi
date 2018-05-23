0a16485ba073d9dd0193ae35f2d329a362168536
Xavi Moll <xmollv@gmail.com>
2018-05-23 22:10:48 +0200

    Improved the handling of localizations

--- llitgi/Extensions & Helpers/L10n.swift
+++ llitgi/Extensions & Helpers/L10n.swift
@@ -11,25 +11,25 @@ import Foundation
 enum L10n {
     
     enum General {
-        static let dismiss = NSLocalizedString("dismiss", comment: "")
-        static let retry = NSLocalizedString("retry", comment: "")
-        static let search = NSLocalizedString("search", comment: "")
-        static let logout = NSLocalizedString("logout", comment: "")
-        static let errorTitle = NSLocalizedString("error_title", comment: "")
-        static let errorDescription = NSLocalizedString("error_description", comment: "")
-        static let pocketError = NSLocalizedString("error_pocket", comment: "")
+        static let dismiss = NSLocalizedString("General.dismiss", comment: "")
+        static let retry = NSLocalizedString("General.retry", comment: "")
+        static let search = NSLocalizedString("General.search", comment: "")
+        static let logout = NSLocalizedString("General.logout", comment: "")
+        static let errorTitle = NSLocalizedString("General.errorTitle", comment: "")
+        static let errorDescription = NSLocalizedString("General.errorDescription", comment: "")
+        static let pocketError = NSLocalizedString("General.errorPocket", comment: "")
     }
     
     enum Add {
-        static let invalidPasteboard = NSLocalizedString("pasteboard_not_valid", comment: "")
+        static let invalidPasteboard = NSLocalizedString("Add.invalidPasteboard", comment: "")
     }
     
     enum Titles {
-        static let all = NSLocalizedString("all", comment: "")
-        static let myList = NSLocalizedString("my_list", comment: "")
-        static let favorites = NSLocalizedString("favorites", comment: "")
-        static let archive = NSLocalizedString("archive", comment: "")
-        static let settings = NSLocalizedString("settings", comment: "")
+        static let all = NSLocalizedString("Titles.all", comment: "")
+        static let myList = NSLocalizedString("Titles.myList", comment: "")
+        static let favorites = NSLocalizedString("Titles.favorites", comment: "")
+        static let archive = NSLocalizedString("Titles.archive", comment: "")
+        static let settings = NSLocalizedString("Titles.settings", comment: "")
     }
     
     enum Onboarding {
@@ -44,48 +44,48 @@ enum L10n {
     }
     
     enum Sync {
-        static let title = NSLocalizedString("sync_title", comment: "")
-        static let explanation = NSLocalizedString("sync_explanation", comment: "")
-        static let successTitle = NSLocalizedString("sucess", comment: "")
-        static let successExplanation = NSLocalizedString("sync_explanation_success", comment: "")
-        static let successButton = NSLocalizedString("lets_go", comment: "")
+        static let title = NSLocalizedString("Sync.title", comment: "")
+        static let explanation = NSLocalizedString("Sync.explanation", comment: "")
+        static let successTitle = NSLocalizedString("Sync.success", comment: "")
+        static let successExplanation = NSLocalizedString("Sync.successExplanation", comment: "")
+        static let successButton = NSLocalizedString("Sync.successButton", comment: "")
     }
     
-    enum ListEmptyStates {
-        static let allTitle = NSLocalizedString("no_results_all_title", comment: "")
-        static let allSubtitle = NSLocalizedString("no_results_all_subtitle", comment: "")
-        static let myListTitle = NSLocalizedString("no_results_myList_title", comment: "")
-        static let myListSubtitle = NSLocalizedString("no_results_myList_subtitle", comment: "")
-        static let favoritesTitle = NSLocalizedString("no_results", comment: "")
-        static let favoritesSubtitle = NSLocalizedString("no_results_favorites_subtitle", comment: "")
-        static let archiveTitle = NSLocalizedString("no_results", comment: "")
-        static let archiveSubtitle = NSLocalizedString("no_results_archive_subtitle", comment: "")
-        static let searchTitle = NSLocalizedString("no_results", comment: "")
-        static let searchSubtitle = NSLocalizedString("no_results_search", comment: "")
+    enum EmptyStates {
+        static let allTitle = NSLocalizedString("EmptyStates.noResults", comment: "")
+        static let allSubtitle = ""
+        static let myListTitle = NSLocalizedString("EmptyStates.myListTitle", comment: "")
+        static let myListSubtitle = NSLocalizedString("EmptyStates.myListSubtitle", comment: "")
+        static let favoritesTitle = NSLocalizedString("EmptyStates.noResults", comment: "")
+        static let favoritesSubtitle = NSLocalizedString("EmptyStates.favoritesSubtitle", comment: "")
+        static let archiveTitle = NSLocalizedString("EmptyStates.noResults", comment: "")
+        static let archiveSubtitle = NSLocalizedString("EmptyStates.archiveSubtitle", comment: "")
+        static let searchTitle = NSLocalizedString("EmptyStates.noResults", comment: "")
+        static let searchSubtitle = NSLocalizedString("EmptyStates.noResultsSubtitle", comment: "")
     }
     
     enum Actions {
-        static let favorite = NSLocalizedString("favorite", comment: "")
-        static let unfavorite = NSLocalizedString("unfavorite", comment: "")
-        static let archive = NSLocalizedString("to_archive", comment: "")
-        static let unarchive = NSLocalizedString("unarchive", comment: "")
-        static let delete = NSLocalizedString("delete", comment: "")
+        static let favorite = NSLocalizedString("Actions.favorite", comment: "")
+        static let unfavorite = NSLocalizedString("Actions.unfavorite", comment: "")
+        static let archive = NSLocalizedString("Actions.archive", comment: "")
+        static let unarchive = NSLocalizedString("Actions.unarchive", comment: "")
+        static let delete = NSLocalizedString("Actions.delete", comment: "")
     }
     
     enum Settings {
-        static let badgeCountTitle = NSLocalizedString("badge_count", comment: "")
-        static let badgeCountExplanation = NSLocalizedString("badge_explanation", comment: "")
-        static let safariOpenerTitle = NSLocalizedString("open_links_safari", comment: "")
-        static let safariOpenerDescription = NSLocalizedString("safari_open_explanation", comment: "")
-        static let safariReaderTitle = NSLocalizedString("safari_reader_mode", comment: "")
-        static let safariReaderDescription = NSLocalizedString("safari_reader_mode_explanation", comment: "")
+        static let badgeCountTitle = NSLocalizedString("Settings.badgecount", comment: "")
+        static let badgeCountExplanation = NSLocalizedString("Settings.badgeExplanation", comment: "")
+        static let safariOpenerTitle = NSLocalizedString("Settings.safariOpenerTitle", comment: "")
+        static let safariOpenerDescription = NSLocalizedString("Settings.safariOpenerExplanation", comment: "")
+        static let safariReaderTitle = NSLocalizedString("Settings.safariReaderTitle", comment: "")
+        static let safariReaderDescription = NSLocalizedString("Settings.safariReaderExplanation", comment: "")
         static let github = NSLocalizedString("Settings.github", comment: "")
         static let twitter = NSLocalizedString("Settings.twitter", comment: "")
-        static let email = NSLocalizedString("email", comment: "")
-        static let buildVersion = NSLocalizedString("build_version", comment: "")
+        static let email = NSLocalizedString("Settings.email", comment: "")
+        static let buildVersion = NSLocalizedString("Settings.buildVersion", comment: "")
     }
     
     enum ShareExtension {
-        static let saving = NSLocalizedString("saving_to_llitgi", comment: "")
+        static let saving = NSLocalizedString("ShareExtension.saving", comment: "")
     }
 }
