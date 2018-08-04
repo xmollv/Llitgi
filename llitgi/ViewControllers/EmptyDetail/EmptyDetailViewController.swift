a0b13b35a8f19e69731205fee012f9fe39c2b225
Adrian Tineo <adrian@adriantineo.com>
2018-08-04 17:42:32 +0200

    Add empty detail when no item selected

---
+++ llitgi/ViewControllers/EmptyDetail/EmptyDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyDetailViewController.swift
+//  llitgi
+//
+//  Created by Adrian Tineo on 04.08.18.
+//  Copyright © 2018 xmollv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EmptyDetailViewController: UIViewController {
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionLabel.text = L10n.EmptyDetail.descriptionTitle
+    }
+    
+}
