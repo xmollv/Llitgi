73dca52755e1f47adfa65e1c72c5a6c70f4fc31e
Xavi Moll <xmollv@gmail.com>
2018-08-24 14:30:51 +0200

    Added a Theme enum and a ThemeManager

---
+++ llitgi/Managers/Theme.swift
@@ -0,0 +1,76 @@
+//
+//  Theme.swift
+//  llitgi
+//
+//  Created by Xavi Moll on 24/08/2018.
+//  Copyright © 2018 xmollv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Theme: String {
+    case light
+    case dark
+    
+    init(withName name: String) {
+        switch name {
+        case "light": self = .light
+        case "dark": self = .dark
+        default: self = .light
+        }
+    }
+    
+    var backgroundColor: UIColor {
+        switch self {
+        case .light: return .white
+        case .dark: return .black
+        }
+    }
+    
+    var textTitleColor: UIColor {
+        switch self {
+        case .light: return .black
+        case .dark: return .white
+        }
+    }
+    
+    var textSubtitleColor: UIColor {
+        switch self {
+        case .light: return UIColor.black.withAlphaComponent(0.75)
+        case .dark: return UIColor.white.withAlphaComponent(0.75)
+        }
+    }
+}
+
+final class ThemeManager {
+    
+    var theme: Theme = .light {
+        didSet {
+            UserDefaults.standard.setValue(theme.rawValue, forKey: "savedTheme")
+            self.themeChanged?(theme)
+        }
+    }
+    var themeChanged: ((_ theme: Theme) -> Void)?
+    
+    init() {
+        self.theme = Theme(withName: UserDefaults.standard.string(forKey: "savedTheme") ?? "")
+    }
+}
+
+final class Test: UIViewController {
+    
+    var themeManager: ThemeManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.apply(theme: themeManager.theme)
+        themeManager.themeChanged = { [weak self] theme in
+            self?.apply(theme: theme)
+        }
+    }
+    
+    func apply(theme: Theme) {
+        self.view.backgroundColor = theme.backgroundColor
+    }
+}
