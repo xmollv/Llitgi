b800599d2a5bfd9ce2498445a26b82ca96671482
Xavi Moll <xmollv@gmail.com>
2018-12-28 23:36:06 +0100

    Removed an unneded check

--- llitgi/ViewControllers/Lists/TaggedItemsViewController.swift
+++ llitgi/ViewControllers/Lists/TaggedItemsViewController.swift
@@ -26,17 +26,16 @@ final class TaggedItemsViewController: BaseListViewController {
 //MARK:- UITableViewDelegate
 extension TaggedItemsViewController {
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return (self.notifier.numberOfSections() > 1) ? UITableView.automaticDimension : 0.0
+        return (self.notifier.numberOfElements(inSection: section) > 0) ? UITableView.automaticDimension : 0.0
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard self.notifier.numberOfSections() > 1 else { return nil }
         let sectionHeaderView = SectionHeaderView(theme: self.themeManager.theme)
         switch section {
-        case 0 where self.notifier.numberOfElements(inSection: section) > 0:
+        case 0:
             sectionHeaderView.text = L10n.Titles.myList
             return sectionHeaderView
-        case 1 where self.notifier.numberOfElements(inSection: section) > 0:
+        case 1:
             sectionHeaderView.text = L10n.Titles.archive
             return sectionHeaderView
         default:
