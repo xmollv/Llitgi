f7f3180e211df586199feaa44e03108345bc2164
Xavi Moll <xmollv@gmail.com>
2018-12-29 20:06:57 +0100

    Reorganized the ItemsList and added the new tagsviewcontroller

---
+++ llitgi/ViewControllers/Tags/TagsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TagsViewController.swift
+//  llitgi
+//
+//  Created by Xavi Moll on 29/12/2018.
+//  Copyright © 2018 xmollv. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+final class TagsViewController: UITableViewController {
+    
+}
