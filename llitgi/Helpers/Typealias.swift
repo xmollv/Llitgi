4c8b3577c60d6b852b464f3a2aa5739c73fdbde7
Xavi Moll <xmollv@gmail.com>
2019-04-07 09:41:21 +0200

    Removed the current result and typealiased the stdlib one

--- llitgi/Helpers/Typealias.swift
+++ llitgi/Helpers/Typealias.swift
@@ -8,18 +8,13 @@
 
 import Foundation
 
+typealias Result<T> = Swift.Result<T, Error>
+
 /// Typealias over Dictionary<String, Any>
 typealias JSONDictionary = Dictionary<String, Any>
 
 /// Typealias over Array<JSONDictionary>
 typealias JSONArray = Array<JSONDictionary>
-
-/// Generic Result type that only allows for a success (with the given generic type)
-/// or an Error on the failure
-enum Result<T> {
-    case success(T)
-    case failure(Error)
-}
 
 enum EmptyResult {
     case success
